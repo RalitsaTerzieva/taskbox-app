94104f4fd227e0973883ff4b008c5f8c5638265f
Ralica Popova <ralicaterzieva90@gmail.com>
2025-02-21 19:38:34 +0200

    Refactor TaskList and add stories

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -7,17 +7,44 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask}) {
         onArchiveTask
     };
 
+    const LoadingRow = (
+        <div className='loading-item'>
+            <span className='glow-checkbox' />
+            <span className='glow-text'>
+                <span>Loading</span><span>cool</span><span>state</span>
+            </span>
+        </div>
+    )
+
     if(loading) {
-        return <div className='list-items'>loading</div>
+        return <div className='list-items' data-testid='loading' key={'loading'}>
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            </div>
     }
 
-    if(tasks.list === 0) {
-        return <div className='list-items'>empty</div>
+    if(tasks?.length === 0) {
+        return <div className='list-items' key={'empty'} data-testid='empty'>
+                <div className='wrapper-message'>
+                    <span className='icon-check'/>
+                    <p className='title-message'>You have no task.</p>
+                    <p className='subtitle-message'>Sit back and relax.</p>
+                </div>
+            </div>
     }
+
+    const taskInOrder = [
+        ...tasks.filter((task) => task.state === 'TASK_PINNED'),
+        ...tasks.filter((task) => task.state !== 'TASK_PINNED'),
+    ]
 
     return (
         <div className='list-items'>
-            {tasks.map(task => {
+            {taskInOrder.map(task => {
                 <Task key={task.id} task={task} {...events}/>
             })}
         </div>
