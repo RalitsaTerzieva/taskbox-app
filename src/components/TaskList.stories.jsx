30660fea949bfb6257f58a7263b02c82a6cdbbea
Ralica Popova <ralicaterzieva90@gmail.com>
2025-02-22 18:31:18 +0200

    Create mock redux store for stories

--- src/components/TaskList.stories.jsx
+++ src/components/TaskList.stories.jsx
@@ -2,11 +2,51 @@ import TaskList from './TaskList';
 
 import * as TaskStories from './Task.stories';
 
+import { Provider } from 'react-redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const MockState = {
+    tasks: [
+        { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+        { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+        { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
+        { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+        { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+        { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+    ],
+    status: 'idle',
+    error: null,
+}
+
+// eslint-disable-next-line react/prop-types
+const MockStore = ({taskboxState, children}) => (
+    <Provider store={configureStore({
+        name: 'taskbox',
+        initialState: taskboxState,
+        reducer: {
+            taskbox: createSlice({
+                reducers: {
+                    updateTaskState: (state, action) => {
+                        const {id, newTaskState} = action.payload;
+                        const task = state.tasks.findIndex((task) => task.id === id);
+                        if(task>=0) {
+                            state.tasks[task].state = newTaskState
+                        }
+                    } 
+                }
+            }).reducer,
+        },
+    })}>
+        {children}
+    </Provider>
+)
+
 const meta = {
   component: TaskList,
   title: 'TaskList',
   decorators: [(story) => <div style={{ margin: '3rem' }}>{story()}</div>],
   tags: ["autodocs"],
+  excludeStories: /.*MockState$/,
   args: {
     ...TaskStories.ActionsData,
   },
@@ -15,18 +55,9 @@ const meta = {
 export default meta;
 
 export const Default = {
-  args: {
-    // Shaping the stories through args composition.
-    // The data was inherited from the Default story in Task.stories.tsx.
-    tasks: [
-      { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
-      { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
-      { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
-      { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
-      { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
-      { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
-    ],
-  },
+ decorators: [
+    (story) => <MockStore taskboxState={MockState}>{story()}</MockStore>
+ ]
 };
 
 export const WithPinnedTasks = {
