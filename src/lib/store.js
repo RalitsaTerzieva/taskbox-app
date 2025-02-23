ed99fa97fcc6dc24241c07008504b3cdaebfa3b4
Ralica Popova <ralicaterzieva90@gmail.com>
2025-02-23 17:59:25 +0200

    Remove default mock data state

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,18 +1,8 @@
 import { configureStore, createSlice, createAsyncThunk, } from "@reduxjs/toolkit";
 
 
-const defaultTasks = [
-    { id: '1', title: 'Task 1', state: 'TASK_INBOX' },
-    { id: '2', title: 'Task 2', state: 'TASK_INBOX' },
-    { id: '3', title: 'Task 3', state: 'TASK_INBOX' },
-    { id: '4', title: 'Task 4', state: 'TASK_ARCHIVED' },
-    { id: '5', title: 'Task 5', state: 'TASK_INBOX' },
-    { id: '6', title: 'Task 6', state: 'TASK_PINNED' },
-  ];
-
-  
 const TaskBoxData = {
-    tasks: defaultTasks,
+    tasks: [],
     status: 'idle',
     error: null
 }
