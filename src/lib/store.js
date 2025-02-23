96428fc161d35f76ba24aca46fd5c3964e8d1b14
Ralica Popova <ralicaterzieva90@gmail.com>
2025-02-23 17:58:19 +0200

    Add inbox screen component and redux thunk logic

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,4 +1,4 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice, createAsyncThunk, } from "@reduxjs/toolkit";
 
 
 const defaultTasks = [
@@ -17,6 +17,19 @@ const TaskBoxData = {
     error: null
 }
 
+export const fetchTasks = createAsyncThunk('todos/fetchTodos', async () => {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/todos?userId=1'
+    );
+    const data = await response.json();
+    const result = data.map((task) => ({
+      id: `${task.id}`,
+      title: task.title,
+      state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX',
+    }));
+    return result;
+  });
+
 const TaskSlice = createSlice({
     name: 'taskbox',
     initialState: TaskBoxData,
@@ -27,11 +40,47 @@ const TaskSlice = createSlice({
             if(task>=0) {
                 state.tasks[task].state = newTaskState
             }
-        } 
-    }
+        },
+        addNewTask: (state, action) => {
+            const { newTask } = action.payload;
+          
+            // Create a new copy of the tasks array
+            const newTasks = [...state.tasks];
+          
+            // Push the new task into the new array
+            newTasks.push({
+              id: state.tasks.length + 1,
+              title: newTask,
+              state: 'TASK_INBOX',
+            });
+          
+            // Assign the new array to the tasks state variable
+            state.tasks = newTasks;
+          },
+    },
+    extraReducers(builder) {
+        builder
+        .addCase(fetchTasks.pending, (state) => {
+          state.status = 'loading';
+          state.error = null;
+          state.tasks = [];
+        })
+        .addCase(fetchTasks.fulfilled, (state, action) => {
+          state.status = 'succeeded';
+          state.error = null;
+          // Add any fetched tasks to the array
+          state.tasks = action.payload;
+         })
+        .addCase(fetchTasks.rejected, (state) => {
+          state.status = 'failed';
+          state.error = "Something went wrong";
+          state.tasks = [];
+        });
+     },
 })
 
 export const {updateTaskState} = TaskSlice.actions;
+export const { addNewTask } = TaskSlice.actions;
 
 export const store = configureStore({
     reducer: {
