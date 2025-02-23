df0d802e0c964d390368d056d74265cff91e49e2
Ralica Popova <ralicaterzieva90@gmail.com>
2025-02-23 19:30:20 +0200

    Use msw in preview file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,23 @@
 import '../src/index.css';
 
+// Registers the msw addon
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+// Initialize MSW
+initialize();
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
-};
+  decorators: [mswDecorator],
+   parameters: {
+     actions: { argTypesRegex: "^on[A-Z].*" },
+     controls: {
+       matchers: {
+         color: /(background|color)$/i,
+         date: /Date$/,
+       },
+     },
+   },
+ };
 
 export default preview;
