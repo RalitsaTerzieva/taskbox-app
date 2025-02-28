dde1d4ef9cc9bba003a3ffc46d6f888c48a16b3f
Ralica Popova <ralicaterzieva90@gmail.com>
2025-02-28 17:55:46 +0200

    Change preview configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,23 +1,23 @@
 import '../src/index.css';
 
 // Registers the msw addon
-import { initialize, mswDecorator } from 'msw-storybook-addon';
+import { initialize, mswLoader } from 'msw-storybook-addon';
 
 // Initialize MSW
 initialize();
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
-  decorators: [mswDecorator],
-   parameters: {
-     actions: { argTypesRegex: "^on[A-Z].*" },
-     controls: {
-       matchers: {
-         color: /(background|color)$/i,
-         date: /Date$/,
-       },
-     },
-   },
+  loaders: [mswLoader],
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
  };
 
 export default preview;
