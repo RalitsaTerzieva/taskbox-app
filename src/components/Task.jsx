57ba12b9dae5673d722d99d8e5cb217d8fe6a342
Ralica Popova <ralicaterzieva90@gmail.com>
2025-03-02 18:24:17 +0200

    Add checkbox custom button logic

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 
-export default function Task({task: {id, title, state}, onPinTask}) {
+export default function Task({task: {id, title, state}, onArchiveTask, onPinTask}) {
+    const isPinnedOrInbox = state === "TASK_INBOX" || state === "TASK_PINNED";
     return (
         <div className={`list-item ${state}`}>
             <label htmlFor="checked" aria-label={`archivedTask-#${id}`} className="checkbox">
                 <input type="checkbox" disabled={true} value={title} readOnly={true} name="checked" id={`archivedTask-#${id}`} checked={state === "TASK_ARCHIVED"} />
+                <span className="checkbox-custom" onClick={() => isPinnedOrInbox ? onArchiveTask(id) : null} />
             </label>
             <label htmlFor="title" aria-label={title} className={title}>
                 <input type="text" value={title} readOnly={true} name="title" placeholder="Input title" />
